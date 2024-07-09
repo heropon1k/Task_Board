9f49956d2cc819ae1d520ef883b4f410836f3f9f
unknown <brian.zhen101@gmail.com>
2024-07-09 13:04:17 -0400

    added some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const title = $('#task-title');
+const date = $('#datepicker');
+const description = $('#task-description');
+let stored = [];
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -9,6 +13,13 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const card = $('<div>').addClass('card zindex task-card draggable my-4').attri('data-task-id', task.id);
+    const cardHeader = $('<div>').addClass('card-header h4').text(task.title);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescript = $('<p>').addClass('card-text').text(task.description);
+    const cardDue = $('<p>').addClass('card-text').text(task.date);
+    const cardDelete = $('<button>').addClass('btn btn-danger delet').text('Delete').attr('data-task-id', task.id)
+    cardDelete.on('click', handleDeletTask);
 
 }
 
@@ -18,12 +29,24 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
+    if (stored !== null) {
+        stored = taskList;
+    }
+    const newTask ={
+        id: crypto.randomUUID(),
+        name: title,
+        date: date,
+        description: description,
+    };
 
+    stored.push(newTask);
+
+    renderTaskList();
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
@@ -34,5 +57,14 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    $('#submit').on("click", function () {
+        handleAddTask();
+        createTaskCard();
+    });
 
 });
+
+$('#datepicker').datepicker({
+    changeMonth: true,
+    changeYear: true,
+})
